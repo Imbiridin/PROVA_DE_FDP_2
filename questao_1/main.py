3ab293cc0eba6ac1c00274c6896c346c67490ffb
Imbiridin <marcosimbiriba1@gmail.com>
2026-07-01 20:42:56 -0300

    Enviando as duas funcoes

--- questao_1/main.py
+++ questao_1/main.py
@@ -1,32 +1,69 @@
 compra = list()
 
-def vendas():
-    while True:
-        print("-" * 60)
-        print("VENDA")
-        print("-" * 60)
+pessoa = list()
 
-        produto   = input("Digite o nome do produto: ")
-        categoria = input("Digite a categoria do produto: ")
-        valor     = float(input("Digite o valor do produto: "))
+# def vendas():
+#     while True:
+#         print("-" * 60)
+#         print("VENDA")
+#         print("-" * 60)
+
+#         produto   = input("Digite o nome do produto: ").lower()
+#         categoria = input("Digite a categoria do produto: ").lower()
+#         valor     = float(input("Digite o valor do produto: "))
 
         
-        compra.append({
-            "produto":   produto,
-            "categoria": categoria,
-            "valor":     valor
-        })
+#         compra.append({
+#             "produto":   produto,
+#             "categoria": categoria,
+#             "valor":     valor
+#         })
 
-        print("-" * 60)
-        continuar = input("Deseja continuar? (s/n): ").strip().lower()
+#         print("-" * 60)
+#         continuar = input("Deseja continuar? (s/n): ").lower()
+
+#         if continuar == "n":
+#             break   
+
+#     print("-"*100)
+#     print("Vendas registradas:")
+#     for i, venda in enumerate(compra, start=1):
+#         print(f"  {i}. {venda['produto']} | {venda['categoria']} | R$ {venda['valor']:.2f}")
+
+
+# if __name__ == "__main__":
+#     vendas()
+
+
+def cliente():
+    
+    while True:
+        
+        print("-"*100)
+        print("FIDELIDADE E IDADE DO CLIENTE")
+        print("-"*100)
+
+        fidelidade = input("Ele é fiel?(s/n): ").lower()
+        idade = int(input("Digite a idade do cliente: "))
+
+
+        pessoa.append({
+            "fidelidade": fidelidade,
+            "idade": idade
+        })
+        
+        print("-"*100)
+        continuar = input("Deseja continuar?(s/n): ").lower()
 
         if continuar == "n":
-            break   
+            break
 
-    print("\nVendas registradas:")
-    for i, venda in enumerate(compra, start=1):
-        print(f"  {i}. {venda['produto']} | {venda['categoria']} | R$ {venda['valor']:.2f}")
-
+    print("-"*100)
+    print("FIDELIDADE E IDADE DO CLIENTE")
+    for i, c in enumerate(pessoa, start=1):
+            print(f" {i}. {cliente[fidelidade]} | {cliente[idade]}")
 
 if __name__ == "__main__":
-    vendas()
+    cliente()
+
+
